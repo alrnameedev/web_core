f9843c46133dc2adf4a99eb6c1141f33e86517c2
alrnamee <bjbjbjbj2600@gmail.com>
2026-05-25 17:52:46 +0200

    initial basic page setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,47 @@
-import Image from "next/image";
-
-export default function Home() {
+const Home = () => {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+    <>
+      <header className="mb-32 flex flex-col items-start">
+        <h1 className="text-3xl font-semibold">Alrnamee</h1>
+        <p className="font-semibold leading-none mt-2">
+          nothing's here, wt you expect??
+        </p>
+      </header>
+
+      <main className="mb-15">
+        <section>
+          <h1 className="relative w-fit px-8 uppercase mx-auto bg-[#e8602e21] text-black/90 border-accent border-[0.5px] text-xl font-machina font-light  pt-1.5  inline-block">
+            What we're
+            <span className="absolute w-0.75 h-0.75 bg-black/60 z-10 top-0 left-0 -translate-x-1/2 -translate-y-1/2"></span>
+            <span className="absolute w-0.75 h-0.75 bg-black/60 z-10 top-0 right-0 translate-x-1/2 -translate-y-1/2"></span>
+            <span className="corner-dot-bl absolute w-0.75 h-0.75 bg-black/60 z-10 bottom-0 left-0 -translate-x-1/2 translate-y-1/2"></span>
+            <span className="corner-dot-br absolute w-0.75 h-0.75 bg-black/60 z-10 bottom-0 right-0 translate-x-1/2 translate-y-1/2"></span>
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+          <p className="mt-5">
+            We offer our end users very personalized products, we invent via
+            what you need.
           </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+          <p className="mt-5">
+            It is indeed a pleasure working with out clients to make our
+            products and help our clients.
+          </p>
+        </section>
       </main>
-    </div>
+
+      <footer className="mb-5">
+        <h1 className="mb-5 relative w-fit px-5 uppercase mx-auto bg-[#e8602e21] text-black/90 border-accent border-[0.5px] text-xl font-machina font-light  pt-1.5  inline-block">
+          contact us via
+          <span className="absolute w-0.75 h-0.75 bg-black/60 z-10 top-0 left-0 -translate-x-1/2 -translate-y-1/2"></span>
+          <span className="absolute w-0.75 h-0.75 bg-black/60 z-10 top-0 right-0 translate-x-1/2 -translate-y-1/2"></span>
+          <span className="corner-dot-bl absolute w-0.75 h-0.75 bg-black/60 z-10 bottom-0 left-0 -translate-x-1/2 translate-y-1/2"></span>
+          <span className="corner-dot-br absolute w-0.75 h-0.75 bg-black/60 z-10 bottom-0 right-0 translate-x-1/2 translate-y-1/2"></span>
+        </h1>
+        <p className="">
+          - Discord <br />
+          - Email <br />
+        </p>
+      </footer>
+    </>
   );
-}
+};
+export default Home;
